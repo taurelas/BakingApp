9616bbd1981613697295ec600deeb5187e116ee5
Matt <mbeimcik@gmail.com>
2018-04-14 13:10:30 +0200

    proper layout dummy data

--- app/src/main/java/com/leadinsource/bakingapp/MainActivity.java
+++ app/src/main/java/com/leadinsource/bakingapp/MainActivity.java
@@ -1,29 +1,44 @@
 package com.leadinsource.bakingapp;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements MainListAdapter.Callback {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridview = findViewById(R.id.gridview);
-        gridview.setAdapter(new MainListAdapter(this));
+        MainActivityViewModel viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager lm = new GridLayoutManager(this, getResources().getInteger(R.integer.main_list_columns));
+        recyclerView.setLayoutManager(lm);
+
+        viewModel.getRecipeNames().observe(this, new Observer<List<String>>() {
+            @Override
+            public void onChanged(@Nullable List<String> recipeNames) {
+                recyclerView.setAdapter(new MainListAdapter(MainActivity.this, recipeNames));
             }
         });
+
+
     }
 
     @Override
